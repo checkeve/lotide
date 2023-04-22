5b41783f6f90606d9e4ef401aa7efabbea198e0e
checkeve <joanna.check@gmail.com>
2023-04-21 21:26:01 -0700

    adding assertArraysEqual.js with function for console logging message saying whether or not two arrays are equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+    //   console.log(array1[i], array2[i])
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === true) {
+    console.log("The two arrays are equal to each other.");
+  } else console.log("The two arrays are not equal.");
+};
+
+assertArraysEqual([1,2,3], [1,2,3]);
+assertArraysEqual(["hi"], ["bye"]);
