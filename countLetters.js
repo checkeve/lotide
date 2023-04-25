5d372f8d55f051779e00099989a5ca600f2c9f9e
checkeve <joanna.check@gmail.com>
2023-04-24 19:57:31 -0700

    Adding countLetters.js with function that can be passed a sentence as the parameter and returns an object which letters occur in the sentence and how many times.

---
+++ countLetters.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`💀💀💀 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let letterObj = {};
+  for (const letter of sentence) {
+    if (letter !== ' ') {
+      if (letterObj[letter]) {
+        letterObj[letter] += 1;
+      } else {
+        letterObj[letter] = 1;
+      }
+    }
+  }
+  return letterObj;
+};
