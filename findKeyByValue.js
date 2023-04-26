3f63b9d708dea6ecd89481696beb836ef39cca5a
checkeve <joanna.check@gmail.com>
2023-04-26 16:46:30 -0700

    Adding findKeyByValue.js with function that accepts an object and value as parameters and returns the first key which contains the given value.

---
+++ findKeyByValue.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`💀💀💀 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function(obj, value) {
+  for (let key in obj) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+};
+
+
+//Testing...
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
+const nums = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4
+};
+assertEqual(findKeyByValue(nums, 3), "three");
+assertEqual(findKeyByValue(nums, 5), undefined);
