c6ee1c7224c5dc57df49e86ff68294cfe2a78abf
checkeve <joanna.check@gmail.com>
2023-04-26 18:04:27 -0700

    Changed console.log messages in the function to be more descriptive and easily visualized with emojis

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,8 +13,8 @@ const eqArrays = function(array1, array2) {
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2) === true) {
-    console.log("The two arrays are equal to each other.");
-  } else console.log("The two arrays are not equal.");
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else console.log(`💀💀💀 Assertion Failed: ${array1} !== ${array2}`);
 };
 
 assertArraysEqual([1,2,3], [1,2,3]);
