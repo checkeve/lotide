a6bd85b2f1b64922fbbdf49e9253d8e069b7f571
checkeve <joanna.check@gmail.com>
2023-05-09 13:02:02 -0700

    Moved test code from assertEqual and head functions into separate files inside test folder. Created an export for assertEqual function so that it is required inside head function instead of previous copy and paste.

--- assertEqual.js
+++ assertEqual.js
@@ -6,7 +6,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(5, -5);
-assertEqual("fun", "fun");
+module.exports = assertEqual;
+
--- head.js
+++ head.js
@@ -1,12 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`💀💀💀 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
-  return array[0]
-}
+  return array[0];
+};
+
+module.exports = head;
+
+
 
