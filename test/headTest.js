2d90d01939c5f35b321b78e861523cda4fdb22ae
checkeve <joanna.check@gmail.com>
2023-05-10 17:31:26 -0700

    Updated heatTest to use Mocha and Chai for running tests on head function.

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,23 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
 
-//Testing...
-assertEqual(head([5, 4, 3, 2, 1]), 5);
-assertEqual(head(["hi", "how", "are", "you"]), "hi");
-assertEqual(head(["hi", "how", "are", "you"]), "you");
+const assert = require('chai').assert;
+
+describe("#head", () => {
+  it("returns 1 for [1,2,3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 5 for [5, 4, 3, 2, 1]", () => {
+    assert.strictEqual(head([5, 4, 3, 2, 1]), 5);
+  });
+  it("returns 'hi' for ['hi', 'how', 'are', 'you']", () => {
+    assert.strictEqual(head(["hi", "how", "are", "you"]), "hi");
+  });
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  })
+  it("returns 'hi' for ['hi']", () => {
+    assert.strictEqual(head(['hi']), 'hi');
+  })
+});
+
