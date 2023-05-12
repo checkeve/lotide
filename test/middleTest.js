b8371caa2d5a1bcfa5915ea054684d34d6f58a22
checkeve <joanna.check@gmail.com>
2023-05-12 16:12:44 -0700

    Refactored middleTest to use mochai and chai.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,20 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-//Testing...
-assertArraysEqual(middle([1, 2, 3, 6, 8, 9]), [3, 6]);
-assertArraysEqual(middle(["go", "stop", "run", "jump", "duck"]), ["run"]);
-assertArraysEqual(middle([1, 0]), []);
-assertArraysEqual(middle(["A"]), []);
-assertArraysEqual(middle([]), []);
+describe("#middle", () => {
+  it("returns [3, 6] for [1, 2, 3, 6, 8, 9]", () => {
+    assert.deepEqual(middle([1, 2, 3, 6, 8, 9]), [3, 6]);
+  });
+  it("returns ['run'] for ['go', 'stop', 'run', 'jump', 'duck']", () => {
+    assert.deepEqual(middle(['go', 'stop', 'run', 'jump', 'duck']), ['run']);
+  });
+  it("returns [] for [1, 0]", () => {
+    assert.deepEqual(middle([1,0]), []);
+  });
+  it("returns [] for ['A']", () => {
+    assert.deepEqual(middle(['A']), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(middle([]), []);
+  });
+});
